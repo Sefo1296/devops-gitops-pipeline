d2ebbe1ee63c199e3da0134a9cfefe927463a35d
Sefo1296 <S.Elsalam07569@student.aast.edu>
2026-07-25 22:18:01 +0300

    Update Flask message

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ APP_VERSION = "1.0.0"
 def home():
     return f"""
     <h1>DevOps GitOps Pipeline</h1>
-    <p>Welcome to our Flask application!</p>
+    <p>Welcome to THE BEST EVER Flask application!</p>
     <p><strong>Version:</strong> {APP_VERSION}</p>
     <p><strong>Hostname:</strong> {socket.gethostname()}</p>
     <p><strong>Time:</strong> {datetime.now()}</p>
